0ad033fcc84fb48ece0ff4379f54242656876aa7
chickooooo <kshitijrajale@gmail.com>
2023-03-06 01:45:55 +0530

    added cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
+from flask_cors import CORS
 from application.components import base, movie_review
 
 # create flask app
 app = Flask(__name__)
+CORS(app)
 
 
 # home route
